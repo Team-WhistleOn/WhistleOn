9ba5891e25a51b1974d115630b34c94ea2dd3806
minkwon4 <minkwon4@naver.com>
2020-08-15 16:14:50 +0900

    ADD team rud methods, https://hallaguys.atlassian.net/browse/WHIS-13

--- whistle-on-backend/src/main/java/com/halaguys/whistleon/service/TeamServiceImpl.java
+++ whistle-on-backend/src/main/java/com/halaguys/whistleon/service/TeamServiceImpl.java
@@ -2,10 +2,14 @@ package com.halaguys.whistleon.service;
 
 import com.halaguys.whistleon.domain.team.Team;
 import com.halaguys.whistleon.domain.team.TeamRepository;
+import com.halaguys.whistleon.dto.request.TeamModifyRequestDto;
 import com.halaguys.whistleon.dto.request.TeamRegisterRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TeamServiceImpl implements TeamService {
@@ -19,6 +23,7 @@ public class TeamServiceImpl implements TeamService {
         this.multipartFileService = multipartFileService;
     }
 
+    //Create
     @Override
     public Team registerTeam(@RequestBody TeamRegisterRequestDto teamRegisterRequestDto) {
 
@@ -31,5 +36,35 @@ public class TeamServiceImpl implements TeamService {
                 .build();
 
         return teamRepository.save(team);
+    }
+
+    //Read
+    @Override
+    public Optional<Team> findTeamById(Long id) {
+        return teamRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Team> findTeamByTeamName(String name) {
+        return teamRepository.findByTeamName(name);
+    }
+
+    @Override
+    public List<Team> findAllTeam() {
+        return teamRepository.findAll();
+    }
+
+    //Update
+    @Override
+    public Team modifyTeam(Long id, TeamModifyRequestDto teamModifyRequestDto) {
+        Team team = findTeamById(id).get();
+        team.update(id,teamModifyRequestDto);
+        return team;
+    }
+
+    //Delete
+    @Override
+    public void removeTeam(Long id) {
+        teamRepository.deleteById(id);
     }
 }
