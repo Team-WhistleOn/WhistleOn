dec7923e26666ea07779ca1107ceabed2249a3f6
eotkd4791 <eotkd4791@gmail.com>
2020-08-16 21:32:24 +0900

    ADD root/index.ts, https://hallaguys.atlassian.net/browse/WHIS-10

--- whistle-on-client/src/store/index.ts
+++ whistle-on-client/src/store/index.ts
@@ -1,15 +1,23 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
+import Vuex, {ActionContext, Commit, Dispatch, StoreOptions} from 'vuex';
+import team from './modules/team';
+import {IRoot} from '../types/interface';
+import state from '@/store/state';
+import getters from '@/store/getters';
+import mutations from '@/store/mutations';
+import actions from '@/store/actions';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+
+const store: StoreOptions<IRoot> = {
   modules: {
+    team,
   },
-});
+  state,
+  getters,
+  mutations,
+  actions,
+};
+
+export default new Vuex.Store(store);
