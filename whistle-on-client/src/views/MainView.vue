84b20592cceb297b955cc172848e396cc51caf55
eotkd4791 <eotkd4791@gmail.com>
2020-08-16 21:26:40 +0900

    ADD MainView.vue, https://hallaguys.atlassian.net/browse/WHIS-10

---
+++ whistle-on-client/src/views/MainView.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <header-nav />
+    <router-view />
+    <footer-nav />
+  </main>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+import HeaderNav from '@/components/HeaderNav.vue';
+import FooterNav from '@/components/FooterNav.vue';
+
+@Component({
+  components: {
+    HeaderNav,
+    FooterNav,
+  },
+})
+
+export default class Main extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
