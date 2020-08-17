b3b334e668d0e10f7cba37649ffb7067fdf7b9c4
eotkd4791 <eotkd4791@gmail.com>
2020-08-18 01:26:42 +0900

    FIX typing error, https://hallaguys.atlassian.net/browse/WHIS-8

--- whistle-on-client/src/api/auth.ts
+++ whistle-on-client/src/api/auth.ts
@@ -2,14 +2,27 @@ import instance from './index';
 import {IUser} from '@/types/interface';
 import {AxiosResponse} from 'axios';
 
-export function userLogin({email, password}: {email: string, password: string}): Promise<AxiosResponse<{msg: string, userName: string}>> {
+export function userLogin(
+  {email, password}: {email: string, password: string})
+  : Promise<AxiosResponse<{
+    headers: {
+      Authorization: string
+    },
+    data: {
+      msg: string,
+      userName: string
+    },
+    status: number
+  }>> {
   return instance.post('/users/login', {email, password});
 }
 
-export function userSignup(newUserInfo: IUser): Promise<AxiosResponse<{msg: string}>> {
+export function userSignup(newUserInfo: IUser)
+  : Promise<AxiosResponse<{data: { msg: string }}>> {
   return instance.post('/users/signup', newUserInfo);
 }
 
-export function userEmailCheck({email}: {email: string}): Promise<AxiosResponse<{msg: string}>> {
+export function userEmailCheck({email}: {email: string})
+  : Promise<AxiosResponse<{ data: { msg: string } }>> {
   return instance.get(`/users/check/${email}`);
 }
