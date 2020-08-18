6cdd4cfa27545d5bfe952fa12c1402879b075786
minkwon4 <minkwon4@naver.com>
2020-08-19 00:48:50 +0900

    UPDATE team controller and service, https://hallaguys.atlassian.net/browse/WHIS-13

---
+++ whistle-on-backend/src/main/java/com/halaguys/whistleon/dto/response/TeamResponseDto.java
@@ -0,0 +1,29 @@
+package com.halaguys.whistleon.dto.response;
+
+import com.halaguys.whistleon.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class TeamResponseDto {
+    private Long teamId;
+    private String teamName;
+    private String location;
+    private String logo;
+    private String description;
+    private List<User> users;
+
+    @Builder
+    public TeamResponseDto(Long teamId, String teamName, String location, String logo, String description, List<User> users) {
+        this.teamId = teamId;
+        this.teamName = teamName;
+        this.location = location;
+        this.logo = logo;
+        this.description = description;
+        this.users = users;
+    }
+}
--- whistle-on-backend/src/main/java/com/halaguys/whistleon/service/TeamService.java
+++ whistle-on-backend/src/main/java/com/halaguys/whistleon/service/TeamService.java
@@ -3,21 +3,26 @@ package com.halaguys.whistleon.service;
 import com.halaguys.whistleon.domain.team.Team;
 import com.halaguys.whistleon.dto.request.TeamModifyRequestDto;
 import com.halaguys.whistleon.dto.request.TeamRegisterRequestDto;
+import com.halaguys.whistleon.dto.response.TeamResponseDto;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface TeamService {
     //Create
-    public Team registerTeam(TeamRegisterRequestDto teamRegisterRequestDto);
+    public void registerTeam(TeamRegisterRequestDto teamRegisterRequestDto);
 
     //Read
     public Optional<Team> findTeamById(Long id);
     public Optional<Team> findTeamByTeamName(String name);
     public List<Team> findAllTeam();
 
+    public TeamResponseDto getTeamById(Long id);
+    public TeamResponseDto getTeamByTeamName(String name);
+    public List<TeamResponseDto> getAllTeam();
+
     //Update
-    public Team modifyTeam(Long id, TeamModifyRequestDto teamModifyRequestDto);
+    public void modifyTeam(Long id, TeamModifyRequestDto teamModifyRequestDto);
 
     //Delete
     public void removeTeam(Long id);
