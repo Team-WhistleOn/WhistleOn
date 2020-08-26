6176259b7698965a0af3b09a054ea19eaecac443
eotkd4791 <eotkd4791@gmail.com>
2020-08-27 04:27:04 +0900

    ADD teamRoutes, https://hallaguys.atlassian.net/browse/WHIS-12

--- whistle-on-client/src/router/index.ts
+++ whistle-on-client/src/router/index.ts
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import VueRouter, {RouteConfig} from 'vue-router';
 import publicRoutes from '@/router/public';
 import userRoutes from '@/router/user';
+import teamRoutes from '@/router/team';
 
 Vue.use(VueRouter);
 
 const routes: RouteConfig[] = [
   ...publicRoutes,
   ...userRoutes,
+  ...teamRoutes
 ];
 
 const router = new VueRouter({
